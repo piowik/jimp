00caa9c2d7835409514c0ce6dede6016dbf226d5
piowik <piotrwiklinski@gmail.com>
2017-03-09 11:54:00 +0100

    init

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -8,7 +8,7 @@ int factorial(int value) {
     return 0;
   else if (value == 0)
     return 1;
-  else if (value > 1)
+  else if (value >= 1)
     return value*factorial(value-1);
   else
     return value*factorial(value+1);
