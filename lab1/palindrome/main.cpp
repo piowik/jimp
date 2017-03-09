1fae7338cf0311bf715e418e1651f0dae6e5c104
piowik <piotrwiklinski@gmail.com>
2017-03-09 11:56:26 +0100

    rozw palindromy

--- lab1/palindrome/main.cpp
+++ lab1/palindrome/main.cpp
@@ -7,12 +7,31 @@
 using namespace std;
 
 int main() {
-    string tekst;
-    cout<<"Wprowadz tekst: ";
-    cin>>tekst;
-    if (is_palindrome(tekst))
-        cout << " jest palindromem" << endl;
-    else
-        cout << " nie jest palindromem";
+    string text;
+    int option;
+    bool exit = false;
+    while (!exit) {
+        cout << "Co chcesz zrobic?: " << endl;
+        cout << "1. Sprawdz palindrom" << endl;
+        cout << "2. Wyjscie" << endl;
+        cin >> option;
+        switch (option) {
+            case 1:
+                cout << "Wprowadz tekst: ";
+                cin >> text;
+                if (is_palindrome(text))
+                    cout << "jest palindromem" << endl;
+                else
+                    cout << "nie jest palindromem" << endl;
+                break;
+
+            case 2:
+                exit = true;
+                break;
+
+            default:
+                cout << "Nieznana opcja. Sprobuj jeszcze raz." << endl;
+        }
+    }
     return 0;
 }
