064806e8bf514bbeb36bba5075cee59f0d51b909
piowik <piotrwiklinski@gmail.com>
2017-03-16 10:31:27 +0100

    CMakeLists fix

--- lab2/greatestproduct/main.cpp
+++ lab2/greatestproduct/main.cpp
@@ -1,4 +1,12 @@
 //
 // Created by Piotrek on 13.03.2017.
 //
+#include <iostream>
 
+using std::cout;
+using std::endl;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello" << endl;
+    return 0;
+}
