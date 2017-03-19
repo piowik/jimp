eeee09f5f391e7af8b4dabd40a31f892262dd9a2
piowik <piotrwiklinski@gmail.com>
2017-03-19 20:42:58 +0100

    minimaltimedifference z zaj

---
+++ lab3/minimaltimedifference/MinimalTimeDifference.h
@@ -0,0 +1,15 @@
+//
+// Created by Piotrek on 16.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
+#define JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
+
+#include <string>
+#include <vector>
+namespace minimaltimedifference {
+    unsigned int ToMinutes(std::string time_HH_MM);
+
+    unsigned int MinimalTimeDifference(std::vector<std::string> times);
+}
+#endif //JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
---
+++ lab3/minimaltimedifference/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Piotrek on 16.03.2017.
+//
+
+#include <iostream>
+#include "MinimalTimeDifference.h"
+
+namespace minimaltimedifference {
+
+    int main(void) {
+        std::cout << MinimalTimeDifference({"1:00", "23:59", "17:56", "14:13", "15:40"}) << std::endl;
+        std::cout << "---" << std::endl;
+    }
+}
