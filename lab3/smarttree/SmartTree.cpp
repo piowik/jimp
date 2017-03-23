ec3236a7c31c87481218aa3aab20bd8f38e643fa
piowik <piotrwiklinski@gmail.com>
2017-03-23 12:43:32 +0100

    rozw smarttree 5/5

--- lab3/smarttree/SmartTree.cpp
+++ lab3/smarttree/SmartTree.cpp
@@ -4,6 +4,9 @@
 
 #include <ostream>
 #include <memory>
+#include <sstream>
+#include <regex>
+#include <cmath>
 #include <iostream>
 #include "SmartTree.h"
 
@@ -22,15 +25,11 @@ namespace datastructures {
     }
 
     unique_ptr<SmartTree> InsertLeftChild(unique_ptr<SmartTree> tree, unique_ptr<SmartTree> left_subtree) {
-        while (tree->left != nullptr)
-            tree = move(tree->left);
         tree->left = move(left_subtree);
         return tree;
     }
 
     unique_ptr<SmartTree> InsertRightChild(unique_ptr<SmartTree> tree, unique_ptr<SmartTree> right_subtree) {
-        while (tree->right != nullptr)
-            tree = move(tree->right);
         tree->right = move(right_subtree);
         return tree;
     }
@@ -47,16 +46,97 @@ namespace datastructures {
     string DumpTree(const unique_ptr<SmartTree> &tree) {
         if (tree == nullptr)
             return "[none]";
-        string retStr = "[";
-        retStr += std::to_string(tree->value);
-        retStr += " " + DumpTree(tree->left);
-        retStr += " " + DumpTree(tree->right);
-        retStr += "]";
-        //std::cout << retStr << std::endl;
-        return retStr;
+        std::stringstream ret;
+        ret << "[";
+        ret << tree->value;
+        ret << " " + DumpTree(tree->left);
+        ret << " " + DumpTree(tree->right);
+        ret << "]";
+        return ret.str();
+    }
+
+
+    std::size_t FindCharInString(string str, char wantedChar, std::size_t startingPos = 0) {
+        std::size_t foundPosition = str.find(wantedChar, startingPos);
+        if (foundPosition != std::string::npos)
+            return foundPosition;
+        else
+            foundPosition = 1;
+        return foundPosition;
     }
 
     unique_ptr<SmartTree> RestoreTree(const string &tree) {
+        if (tree == "[none]")
+            return nullptr;
+        unique_ptr<SmartTree> root = nullptr;
+        std::size_t currentIndex = 0;
+        int depth = 0;
+        string path = "";
+        int depthIterator = 0;
+        bool leftNow = true;
 
+        while (currentIndex < tree.length() - 1) {
+            std::size_t openingPos = FindCharInString(tree, '[', currentIndex);
+            if (openingPos == -1) // done
+                break;
+            std::size_t closingPos = FindCharInString(tree, ']', currentIndex);
+            if (openingPos < closingPos) {
+                currentIndex = openingPos + 1;
+            } else if (openingPos > closingPos) {
+                depthIterator++;
+                if (depthIterator == 2) { // 2x none
+                    depth--;
+                    depthIterator = 0;
+                }
+                currentIndex = closingPos + 1;
+            }
+
+            char chAt = tree.at(currentIndex);
+            while (chAt == '[' || chAt == ']' || chAt == ' ') { // rare, skip to next digit/none
+                currentIndex++;
+                chAt = tree.at(currentIndex);
+                if (chAt == ']')
+                    depth--;
+            }
+            std::size_t spacePos = FindCharInString(tree, ' ', currentIndex);
+            std::size_t closingPPos = FindCharInString(tree, ']', currentIndex);
+            if (closingPPos < spacePos) { // none
+                leftNow = false;
+                currentIndex += 4;
+            } else {
+                int wart = stoi(tree.substr(currentIndex, spacePos));
+                depth += 1;
+                if ((path.length() == 1) && (depth == 1))
+                    path = "";
+                else
+                    path = path.substr(0, depth - 2);
+                if (root == nullptr) {
+                    root = CreateLeaf(wart);
+                    leftNow = true;
+                } else {
+                    if (leftNow) {
+                        if (path.length() > 0) {
+                            if (path.at(0) == 'L')
+                                root->left = InsertLeftChild(move(root->left), CreateLeaf(wart));
+                            else
+                                root->right = InsertLeftChild(move(root->right), CreateLeaf(wart));
+                        } else
+                            root = InsertLeftChild(move(root), CreateLeaf(wart));
+                        path += "L";
+                    } else {
+                        leftNow = true;
+                        if (path.length() > 0) {
+                            if (path.at(0) == 'L')
+                                root->left = InsertRightChild(move(root->left), CreateLeaf(wart));
+                            else
+                                root->right = InsertRightChild(move(root->right), CreateLeaf(wart));
+                        } else
+                            root = InsertRightChild(move(root), CreateLeaf(wart));
+                        path += "R";
+                    }
+                }
+            }
+        }
+        return root;
     }
 }
